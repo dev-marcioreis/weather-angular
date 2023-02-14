64ec5d846cbb79b31a7e4c9d051447fec6867449
Marcio Reis <dev.marcioreis@gmail.com>
2023-02-14 09:03:55 -0300

    Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WeatherWidgetMainComponent } from './Components/weather-widget-main/weather-widget-main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherWidgetMainComponent
   ],
   imports: [
     BrowserModule
